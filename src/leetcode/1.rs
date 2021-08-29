628b2735ca6cb48e07ba7f569bc8c82bd499d868
Tim <chipsleten@gmail.com>
2021-08-29 17:40:49 +0200

    solution for 1

---
+++ src/leetcode/1.rs
@@ -0,0 +1,17 @@
+use std::collections::HashMap;
+use std::io;
+
+fn main() {
+    two_sum(vec![3, 2, 4], 6);
+}
+
+pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+    for (idx, i) in nums.iter().enumerate() {
+        for (ndx, n) in nums.iter().enumerate() {
+            if i + n == target && idx != ndx {
+                return vec![idx as i32, ndx as i32];
+            }
+        }
+    }
+    unreachable!()
+}
